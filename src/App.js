bdf09c851febfbd15f136c84258d95a88fa65c32
aksu <Scyzhe@hotmail.com>
2022-04-06 11:11:10 +0300

    7a(73)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 
+
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link, useNavigate, useMatch, useLocation, useParams
+} from "react-router-dom"
+
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to='/anecdotes'>anecdotes</Link>
+      <Link style={padding} to='/new'>create new</Link>
+      <Link style={padding} to='/about'>about</Link>
     </div>
   )
 }
@@ -17,7 +24,7 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )
@@ -49,6 +56,7 @@ const CreateNew = (props) => {
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
 
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -58,6 +66,7 @@ const CreateNew = (props) => {
       info,
       votes: 0
     })
+    navigate('/anecdotes')
   }
 
   return (
@@ -74,7 +83,7 @@ const CreateNew = (props) => {
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
       </form>
@@ -122,15 +131,36 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const Anecdote = (props) => {
+    const params = useParams()
+    const anecdote = anecdoteById(Number(params.id))
+
+    return (
+      <div>
+        <h2>{anecdote.content}</h2>
+        <div>{anecdote.author}</div>
+        <a href={anecdote.info}>{anecdote.info}</a>
+        <div>votes: {anecdote.votes}</div>
+      </div>
+    )
+  }
   return (
-    <div>
+    <Router>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+
+      <div>
+        <Routes>
+          <Route path='/anecdotes/:id' element={<Anecdote />} />
+          <Route path='/anecdotes' element={<AnecdoteList anecdotes={anecdotes} />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/new' element={<CreateNew addNew={addNew} />} />
+        </Routes>
+      </div>
+
+      <br />
       <Footer />
-    </div>
+    </Router>
   )
 }
 
